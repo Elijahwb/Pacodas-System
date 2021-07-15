04511bc1b61527bd6128b3adb5061c24b148e104
Elijahwb <brandonelijah099@gmail.com>
2021-07-15 13:46:25 +0300

    ui and the backend setup of the project

---
+++ ui/src/components/MainFrame.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main-container">
+        
+        <slot></slot>
+        
+    </div>
+</template>
+
+<style scoped>
+.main-container {
+    background: orange;
+}
+</style>
---
+++ ui/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <MainFrame>
+        <div class="container">
+            <h2>Landing Page</h2>
+        </div>
+    </MainFrame>
+</template>
+<script>
+import MainFrame from '@/components/MainFrame.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        MainFrame,
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    height: 130vh;
+    background: #fff;
+}
+</style>
